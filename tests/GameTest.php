17309bc60f2d2a3013ebdf5208900d46964772df
Mercedes <mvazquezsan@gmail.com>
2020-11-28 14:58:27 +0100

    scissors and spock tests added

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -60,7 +60,20 @@ class GameTest extends TestCase {
         $this->assertEquals($expected, $result);
       
     }
+    public function test_return_tied_paper() 
+   
+    {
+        // given
+        $game = new Game("paper", "paper");
 
+        // when
+        $result = $game->play();
+
+        // then
+        $expected = "tied game";
+        $this->assertEquals($expected, $result);
+
+    }
     public function test_return_paper_wins_rock()
     {
         // given
@@ -89,20 +102,6 @@ class GameTest extends TestCase {
       
     }
 
-    public function test_return_tied_paper() 
-   
-    {
-        // given
-        $game = new Game("paper", "paper");
-
-        // when
-        $result = $game->play();
-
-        // then
-        $expected = "tied game";
-        $this->assertEquals($expected, $result);
-
-    }
 
     public function test_return_paper_defeated_by_lizzard()
     {
@@ -130,6 +129,19 @@ class GameTest extends TestCase {
        $this->assertEquals($expected, $result); 
     }
 
+    public function test_return_tied_lizzard() 
+    {
+        // given
+        $game = new Game("lizzard", "lizzard");
+
+        // when
+        $result = $game->play();
+
+        // then
+        $expected = "tied game";
+        $this->assertEquals($expected, $result);
+
+    } 
     public function test_return_lizzard_wins_paper()
     {
         // given
@@ -158,20 +170,6 @@ class GameTest extends TestCase {
       
     }
 
-    public function test_return_tied_lizzard() 
-    {
-        // given
-        $game = new Game("lizzard", "lizzard");
-
-        // when
-        $result = $game->play();
-
-        // then
-        $expected = "tied game";
-        $this->assertEquals($expected, $result);
-
-    } 
-
     public function test_return_lizzard_defeated_by_rock()
     {
        // given
@@ -198,4 +196,130 @@ class GameTest extends TestCase {
        $this->assertEquals($expected, $result); 
     }
 
+    public function test_return_tied_scissors() 
+    {
+        // given
+        $game = new Game("scissors", "scissors");
+
+        // when
+        $result = $game->play();
+
+        // then
+        $expected = "tied game";
+        $this->assertEquals($expected, $result);
+    } 
+    public function test_return_scissors_win_paper()
+    {
+        // given
+        $game = new Game("scissors", "paper");
+
+        // when
+        $result = $game->play();
+
+        // then
+        $expected = "scissors wins";
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_return_scissors_win_lizzard()
+    {
+       // given
+       $game = new Game("scissors", "lizzard");
+
+       // when
+       $result = $game->play();
+
+       // then
+       $expected = "scissors win";
+       $this->assertEquals($expected, $result); 
+    }
+
+    public function test_return_scissors_defeated_by_spock()
+    {
+        // given
+        $game = new Game("scissors", "spock");
+
+        // when
+        $result = $game->play();
+
+        // then
+        $expected = "spock win";
+        $this->assertEquals($expected, $result);
+    }
+    public function test_return_scissors_defeated_by_rock()
+    {
+       // given
+       $game = new Game("scissors", "rock");
+
+       // when
+       $result = $game->play();
+
+       // then
+       $expected = "rock wins";
+       $this->assertEquals($expected, $result); 
+    }
+    public function test_return_tied_spock() 
+    {
+        // given
+        $game = new Game("spock", "spock");
+
+        // when
+        $result = $game->play();
+
+        // then
+        $expected = "tied game";
+        $this->assertEquals($expected, $result);
+    } 
+    public function test_return_spock_wins_scissors()
+    {
+        // given
+        $game = new Game("spock", "scissors");
+
+        // when
+        $result = $game->play();
+
+        // then
+        $expected = "spock win";
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_return_spock_wins_rock()
+    {
+       // given
+       $game = new Game("spock", "rock");
+
+       // when
+       $result = $game->play();
+
+       // then
+       $expected = "spock wins";
+       $this->assertEquals($expected, $result); 
+    }
+
+    public function test_return_spock_defeated_by_lizzard()
+    {
+        // given
+        $game = new Game("spock", "lizzard");
+
+        // when
+        $result = $game->play();
+
+        // then
+        $expected = "lizzard win";
+        $this->assertEquals($expected, $result);
+    }
+    public function test_return_spock_defeated_by_paper()
+    {
+       // given
+       $game = new Game("spock", "paper");
+
+       // when
+       $result = $game->play();
+
+       // then
+       $expected = "paper wins";
+       $this->assertEquals($expected, $result); 
+    }
+
+    
 }
