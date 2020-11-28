7ed2d62378163818fbc224363154977c9877706d
Mercedes <mvazquezsan@gmail.com>
2020-11-28 13:02:34 +0100

    unifiying game and rules classes

--- src/Models/Game.php
+++ src/Models/Game.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-class Game extends Rules implements iPlay
+class Game 
 {
-    public string $result;
+    private string $result;
 
     public function __construct(string $choice1, string $choice2)
     {
@@ -14,7 +14,58 @@ class Game extends Rules implements iPlay
 
     public function play()
     {
-       $result =  $this->startGame($this->choice1, $this->choice2);
-       return $result;
+      $this->startGame($this->choice1, $this->choice2);
+       return $this->getResult();
     }
+
+    private function startGame($choice1, $choice2)
+    {
+
+        if ($choice1 === $choice2){
+            $this->result = 'tied game';
+            return $this->getResult();
+        }
+        
+        if ($choice1 === 'rock')
+        {
+            $rock = new Rock($choice2);
+            $this->result = $rock->result;
+            return $this->getResult();
+        }
+
+       if ($choice1 === 'paper')
+        {
+            $paper= new Paper($choice2);
+            $this->result = $paper->result;
+            return $this->getResult();
+        }
+
+        if ($choice1 === 'scissors'){
+
+            $scissors= new Scissors($choice2);
+            $this->result = $scissors->result;
+            return $this->getResult();
+        }
+
+        if ($choice1 === 'lizzard'){
+           
+            $lizzard = new Lizzard($choice2);
+            $this->result = $lizzard->result;
+            return $this->getResult();
+        }
+
+        if ($choice1 === 'spock'){
+            
+            $spock = new Spock ($choice2);
+            $this->result = $spock->result;
+            return $this->getResult();
+        }
+    
+    }
+    
+    private function getResult()
+    {
+        return $this->result;
+    }
+    
 }
